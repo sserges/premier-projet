f795e692d1440b0fe286a218c2b08d87c37abe1a
sserges <agounkeserge@gmail.com>
2019-08-05 19:10:14 +0000

    use the style inline

--- src/components/Membre.js
+++ src/components/Membre.js
@@ -3,7 +3,12 @@ import React, { Fragment } from "react";
 const Membre = ({ nom, age, children }) => {
   return (
     <Fragment>
-      <h2>
+      <h2
+        style={{
+          backgroundColor: age < 10 ? "yellow" : "purple",
+          color: age < 10 ? "black" : "white"
+        }}
+      >
         {nom.toUpperCase()} : {age}
       </h2>
       {children ? <p>{children}</p> : <Fragment />}
